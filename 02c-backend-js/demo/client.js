95d518468313e3415d86e692bb48b5ed462401b8
Andreas Hirsch <andreas.hirsch@baitando.com>
2022-04-24 23:09:27 +0200

    02c Backend Javascript demo

---
+++ 02c-backend-js/demo/client.js
@@ -0,0 +1,16 @@
+var http = require('http');
+
+http.get('http://localhost:8080', (resp) => {
+    let data = '';
+
+    resp.on('data', (chunk) => {
+        data += chunk;
+    });
+
+    resp.on('end', () => {
+        console.log(data);
+    });
+
+}).on("error", (err) => {
+    console.log(err.message);
+});
---
+++ 02c-backend-js/demo/greeter.js
@@ -0,0 +1,3 @@
+exports.greeting = function () {
+    return 'Hello World!';
+};
