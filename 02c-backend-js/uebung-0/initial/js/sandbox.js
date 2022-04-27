13ea84e78b359cf642ac60943255c58dbdcd4ae5
Andreas Hirsch <andreas.hirsch@baitando.com>
2022-04-27 16:31:31 +0200

    02a Uebung 0

---
+++ 02c-backend-js/uebung-0/initial/js/sandbox.js
@@ -0,0 +1,15 @@
+function run() {
+    // TODO Text "Hello World!" auf der Konsole ausgeben
+
+    // TODO Neue Funktion zur Addition von 2 Eingabewerten hinzufuegen
+
+    // TODO Die Funktion zur Addition aufrufen und das Ergebnis der Berechnung auf der Konsole ausgeben
+
+    // TODO Das Ergebnis der Addition als String im Local Storage abspeichern
+
+    // TODO Das Erebnis der Addition sowie die beiden Eingabewerte als JSON-String in den Local Storage speichern
+
+    // TODO Den JSON-String aus dem Local Storage auslesen und auf der Konsole ausgeben
+
+    // TODO Aufruf der REST API zur Abfrage eines einzelnen Tasks und Ausgabe des Ergebnisses als JSON auf die Konsole
+}
