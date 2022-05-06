ca5d080d92826284564cbd0dd3fe646d67db18bb
Andreas Hirsch <andreas.hirsch@baitando.com>
2022-05-06 21:39:27 +0200

    Exercise 1 module 2c

--- 02c-backend-js/uebung-1/initial/server.js
+++ 02c-backend-js/uebung-1/initial/server.js
@@ -1,30 +1,41 @@
-var http = require("http");
+const http = require("http");
 
-var data = {
-    // TODO #1 Daten fuer die Rueckgabe definieren
+/*
+TODO #1 Daten fuer die Rueckgabe definieren
+
+Definieren Sie hier statische Beispieldaten, die spaeter bei der Abfrage der Liste aller Eintraege als Ergebnis
+geliefert wird. Als Inspiration koennen Sie die Swagger UI der vorhandenen REST API aufrufen und darueber einen
+Aufruf durchfuehren und sich das Ergebnis ansehen. Sie finden Sie unter der folgenden URL:
+https://editor.swagger.io/?url=https://raw.githubusercontent.com/baitando/dhbw-web/master/02d_apis/uebung-1/initial/todo.yaml
+a) Definieren Sie ein Array.
+b) Fuegen Sie in diesem Array mehrere Tasks-Eintraege ein. Jeder Task-Eintrag ist ein eigenes Objekt.
+ */
+const responseData = {
+
 };
 
 http
     .createServer(function (req, res) {
         console.log("Serving request");
 
-        // TODO #4 CORS-Header setzen
-        // * Header Access-Control-Allo-Origin auf * setzen
-        // * Header Access-Control-Allow-Headers auf * setzen
+        /*
+        TODO #2 Vorab definierte Daten zuerueckgeben, wenn Pfad /tasks entspricht und Methode GET ist
 
-        // TODO #5 Wenn Methode OPTIONS ist, 200 OK als Ergebnis
-        // * Status auf 200 setzen
-        // * Ergebnis zurueckgeben
-
-        // TODO #2 Vorab definierte Daten zuerueckgeben, wenn Pfad /tasks entspricht und Methode GET ist
-        // * Header Content-Type auf application/json setzen
-        // * Status auf 200 setzen
-        // * Daten aus dem Javascript-Objekt in JSON umwandeln und in die Ausgabe schreiben
-        // * Ergebnis zurueck geben
-
-        // TODO #3 In allen anderen Faellen 404 Not Found als Ergebnis
-        // * Status auf 404 setzen
-        // * Ergebnis zurueckgeben
+        a) Erstellen Sie eine Bedinungspruefung mit if und else. Pruefen Sie im if, ob der Pfad '/tasks' mit der HTTP-
+           Methode 'GET' aufgerufen wurde. Die tatsaechlichen Werte des konkreten Aufrufs koennen Sie ueber die
+           Attribute des Parameters req ermitteln.
+        b) Falls die Bedingung zutrifft (d.h. im if-Teil) setzen Sie ueber die Methoden des Parameters res die Antwort.
+           Setzen Sie den Header 'Content-Type' auf den Wert 'application/json', den HTTP-Status auf 200 und schreiben
+           Sie die JSON-Repraesentation des Wertes von responseData in den HTTP-Body.
+        c) Falls die Bedingung nicht zutrifft (d.h. im else-Teil) setzen Sie den HTTP-Status auf 404.
+        d) Testen Sie nun den if-Zweig durch einen Aufruf in Ihrem Browser. Rufen Sie den Pfad /tasks auf. Beachten Sie,
+           dass im Falle Code Sandbox trotz Port 8080 des gestarteten Servers der Aufruf ueber das Internet per Standard
+           HTTPS-Port 443 erfolgt. Sehen Sie sich den Aufruf und die Antwort auch in den Entwicklerwerkzeugen Ihres
+           Browsers an.
+        e) Testen Sie nun den else-Zweig durch einen weiteren Aufruf in Ihrem Browser. Rufen Sie dazu z.B. den Pfad
+           /gibtsnicht auf. Sehen Sie sich den Aufruf und die Antwort auch in den Entwicklerwerkzeugen Ihres Browsers
+           an.
+         */
 
     })
     .listen(8080); //the server object listens on port 8080
--- 02c-backend-js/uebung-1/result/server.js
+++ 02c-backend-js/uebung-1/result/server.js
@@ -1,6 +1,16 @@
-var http = require("http");
+const http = require("http");
 
-var data = {
+/*
+TODO #1 Daten fuer die Rueckgabe definieren
+
+    Definieren Sie hier statische Beispieldaten, die spaeter bei der Abfrage der Liste aller Eintraege als Ergebnis
+    geliefert wird. Als Inspiration koennen Sie die Swagger UI der vorhandenen REST API aufrufen und darueber einen
+    Aufruf durchfuehren und sich das Ergebnis ansehen. Sie finden Sie unter der folgenden URL:
+    https://editor.swagger.io/?url=https://raw.githubusercontent.com/baitando/dhbw-web/master/02d_apis/uebung-1/initial/todo.yaml
+    a) Definieren Sie ein Array.
+    b) Fuegen Sie in diesem Array mehrere Tasks-Eintraege ein. Jeder Task-Eintrag ist ein eigenes Objekt.
+ */
+const data = {
     items: [
         {
             id: "task-1",
@@ -23,13 +33,25 @@ http
     .createServer(function (req, res) {
         console.log("Serving request");
 
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader("Access-Control-Allow-Headers", "*");
+        /*
+        TODO #2 Vorab definierte Daten zuerueckgeben, wenn Pfad /tasks entspricht und Methode GET ist
 
-        if (req.method === 'OPTIONS') {
-            res.writeHead(200);
-            res.end();
-        } else if (req.url === "/tasks" && req.method === "GET") {
+        a) Erstellen Sie eine Bedinungspruefung mit if und else. Pruefen Sie im if, ob der Pfad '/tasks' mit der HTTP-
+           Methode 'GET' aufgerufen wurde. Die tatsaechlichen Werte des konkreten Aufrufs koennen Sie ueber die
+           Attribute des Parameters req ermitteln.
+        b) Falls die Bedingung zutrifft (d.h. im if-Teil) setzen Sie ueber die Methoden des Parameters res die Antwort.
+           Setzen Sie den Header 'Content-Type' auf den Wert 'application/json', den HTTP-Status auf 200 und schreiben
+           Sie die JSON-Repraesentation des Wertes von responseData in den HTTP-Body.
+        c) Falls die Bedingung nicht zutrifft (d.h. im else-Teil) setzen Sie den HTTP-Status auf 404.
+        d) Testen Sie nun den if-Zweig durch einen Aufruf in Ihrem Browser. Rufen Sie den Pfad /tasks auf. Beachten Sie,
+           dass im Falle Code Sandbox trotz Port 8080 des gestarteten Servers der Aufruf ueber das Internet per Standard
+           HTTPS-Port 443 erfolgt. Sehen Sie sich den Aufruf und die Antwort auch in den Entwicklerwerkzeugen Ihres
+           Browsers an.
+        e) Testen Sie nun den else-Zweig durch einen weiteren Aufruf in Ihrem Browser. Rufen Sie dazu z.B. den Pfad
+           /gibtsnicht auf. Sehen Sie sich den Aufruf und die Antwort auch in den Entwicklerwerkzeugen Ihres Browsers
+           an.
+         */
+        if (req.url === "/tasks" && req.method === "GET") {
             res.setHeader("Content-Type", "application/json");
             res.writeHead(200);
             res.write(JSON.stringify(data));
