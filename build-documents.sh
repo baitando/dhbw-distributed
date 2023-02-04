1b279e5204912f275245b05737a8fb31ae9702ff
Andreas Hirsch <andreas.hirsch@baitando.com>
2023-02-04 14:39:59 -0400

    Introduce asciidoctor

--- build-documents.sh
+++ build-documents.sh
@@ -1,13 +1,20 @@
-BUILD_COMMAND=$1
-if [ -z "$BUILD_COMMAND" ]; then
-  BUILD_COMMAND="docker run -it -v $PWD:/documents/ asciidoctor/docker-asciidoctor:1.38 asciidoctor-pdf"
+PDF_CMD=$1
+HTML_CMD=$2
+if [ -z "$PDF_CMD" ]; then
+  PDF_CMD="docker run -it -v $PWD:/documents/ asciidoctor/docker-asciidoctor:1.38 asciidoctor-pdf"
+fi
+
+if [ -z "$HTML_CMD" ]; then
+  HTML_CMD="docker run -it -v $PWD:/documents/ asciidoctor/docker-asciidoctor:1.38 asciidoctor"
 fi
 
 OUT_DIR="out"
-mkdir -p $OUT_DIR
+OUT_DIR_HTML="$OUT_DIR/html"
+OUT_DIR_PDF="$OUT_DIR/pdf"
+rm -rf $OUT_DIR
+mkdir -p $OUT_DIR_HTML
+mkdir -p $OUT_DIR_PDF
 
-function build {
-  $BUILD_COMMAND -a pdf-theme=theme.yml -D "$OUT_DIR" "$1"/"$1".adoc
-}
 
-build 00-intro
+$HTML_CMD -D "$OUT_DIR_HTML" index.adoc
+$PDF_CMD -a pdf-theme=theme.yml -D "$OUT_DIR_PDF" index.adoc
